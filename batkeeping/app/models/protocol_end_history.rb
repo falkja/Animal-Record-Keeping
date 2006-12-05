1892c1f7d52de09d924bf464106988bf8860fc82
Kaushik Ghose <kaushik.ghose@gmail.com>
2006-12-05 23:14:47 +0000

    preparing to work on protocol histories

--- batkeeping/app/models/protocol_end_history.rb
+++ batkeeping/app/models/protocol_end_history.rb
@@ -1,2 +1,6 @@
 class ProtocolEndHistory < ActiveRecord::Base
+    belongs_to :bat
+    belongs_to :protocol
+    belongs_to :user
+    belongs_to :protocol_start_hsitory
 end
--- batkeeping/app/models/protocol_start_history.rb
+++ batkeeping/app/models/protocol_start_history.rb
@@ -1,2 +1,6 @@
 class ProtocolStartHistory < ActiveRecord::Base
+    belongs_to :bat
+    belongs_to :protocol
+    belongs_to :user
+    has_one :protocol_end_history
 end

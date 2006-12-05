eeea0e203e614b6fae9e7f10ba1cbe60b9af64e5
Kaushik Ghose <kaushik.ghose@gmail.com>
2006-12-05 17:45:51 +0000

    only active users

--- batkeeping/app/controllers/main_controller.rb
+++ batkeeping/app/controllers/main_controller.rb
@@ -3,6 +3,12 @@ class MainController < ApplicationController
 	@users = User.find(:all, :conditions => "end_date is null", :order => "name")
   end
   
+  #just show all cage changes
+  def cage_change_histories
+    @cih = CageInHistory.find(:all, :order => "date")
+    @coh = CageOutHistory.find(:all, :order => "date")
+  end
+  
   def login
 	session[:person] = User.find(params[:user][:id])
 	redirect_to :action => 'index'

3d45b9327c33e0681f25baaa6578e54fb29e5b90
Kaushik Ghose <kaushik.ghose@gmail.com>
2007-01-06 04:08:18 +0000

    self filters for three categories of tasks

--- batkeeping/app/models/task.rb
+++ batkeeping/app/models/task.rb
@@ -2,4 +2,17 @@ class Task < ActiveRecord::Base
   has_and_belongs_to_many :users
   belongs_to :cage
   belongs_to :proposed_treatment
+
+  def self.general_tasks
+    find :all, :conditions => 'cage_id is null and proposed_treatment_id is null'
+  end
+  
+  def self.cage_tasks
+    find :all, :conditions => 'cage_id is not null'
+  end
+  
+  def self.medical_tasks
+    find :all, :conditions => 'proposed_treatment_id is not null'    
+  end
+  
 end

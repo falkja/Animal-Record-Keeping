7fa7f1d515a177353d06cd4d1c69552fe8a5bd5d
Ben Falk <falk.ben@gmail.com>
2007-01-14 17:30:10 +0000

    Fixed some bugs.

--- batkeeping/app/models/task.rb
+++ batkeeping/app/models/task.rb
@@ -30,59 +30,53 @@ class Task < ActiveRecord::Base
     today = Time.now.yday
     last_done_day = last_done_date.yday
     
+    #to fix for the last done date being in the last year
     year_done = last_done_date.year
     while year_done < Date.today.year
-      last_done_day = last_done_day - 365
+      Date.leap?(year_done) ? last_done_day = last_done_day - 366 : last_done_day = last_done_day - 365
       year_done = year_done + 1
     end
     
     if repeat == 0 #daily tasks
-      if last_done_day < today #we will get problems at the beginning of a new year
-          return false
+      if last_done_day < today
+        return false
       else
-          return true
+        return true
       end
     else #a particular day of the week
-      post = self.find_post
+      post = self.find_post #gives a year day for which the task needs to be completed by
       
       if  (last_done_day >= post)
-          return true
-        else
-          return false
-        end
+        return true
+      else
+        return false
       end
+    end
       
+  end
+    
+  def find_post
+    today = Time.now.yday
+    today_weekday = Time.now.wday
+    
+    repeat_weekday = repeat - 1 #to bring it in line with normal days of the week: 0-6
+    
+    offset = today_weekday - repeat_weekday #offset is how many days apart today is from our deadline
+    
+    if internal_description == "feed" #feeding tasks can only be done on the deadline day
+      jitter = 0
+    else #everything else can be done one day ahead of schedule
+      jitter = -1
     end
     
-    def find_post
-      today = Time.now.yday
-      today_weekday = Time.now.wday
-      last_done_day = last_done_date.yday
-      last_done_weekday = last_done_date.wday
-      repeat_weekday = repeat - 1 #brings it in line with Time.wday convention
-      
-      done_year = last_done_date.year
-      while done_year < Date.today.year
-        last_done_day = last_done_day - 365
-        done_year = done_year + 1
-      end
-      
-      offset = today_weekday - repeat_weekday #offset is how many days apart today is from our deadline
-      
-      if internal_description == "feed" #feeding tasks can only be done on the deadline day
-        jitter = 0
-      else #everything else can be done one day ahead of schedule
-        jitter = -1
-      end
-      
-      if offset < jitter #because we always want to look back in time, unless you are within you're jitter
-         offset = offset + 7
-      end
-      
-      repeat_day = today - offset #repeat day is finally set, we will get problems if we are in the first week of a new year
-      
-      post = repeat_day + jitter
-      
-      return post
+    if offset < jitter #because we always want to look back in time, unless you are within your jitter
+      offset = offset + 7
     end
+    
+    repeat_day = today - offset #repeat day is finally set, we will get problems if we are in the first week of a new year
+    
+    post = repeat_day + jitter
+    
+    return post
+  end
 end

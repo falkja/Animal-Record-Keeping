0ba9efeba597a8821e47afbc259341c841df7853
Ben Falk <falk.ben@gmail.com>
2007-06-13 15:49:24 +0000

    Fixed a bug where new bats weren't updating the census page correctly.

--- batkeeping/app/models/census.rb
+++ batkeeping/app/models/census.rb
@@ -4,9 +4,10 @@ class Census < ActiveRecord::Base
 		def tally(num_bats, room)
 			self.room = room
 			if self.animals != nil
+        sdf
 				self.animals = self.animals + num_bats
 			else
-				last_census = Census.find(:first, :order => 'date desc', :conditions => 'room_id = ' + room.id.to_s)
+				last_census = Census.find(:first, :order => 'date desc', :conditions => "room_id = #{room.id.to_s} and date < '#{Date.today.to_s}'")
 				if last_census && last_census.animals
           self.animals = last_census.animals + num_bats
         else 

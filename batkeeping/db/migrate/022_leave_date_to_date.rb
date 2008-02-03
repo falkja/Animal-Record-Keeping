e3e40fe4dfb4d3d908d106b2b9684a6407fc5ec4
Ben Falk <falk.ben@gmail.com>
2008-02-03 15:59:10 +0000

    created a few db migrations to convert datetime to date in the database

---
+++ batkeeping/db/migrate/022_leave_date_to_date.rb
@@ -0,0 +1,14 @@
+class LeaveDateToDate < ActiveRecord::Migration
+  def self.up
+		change_column("bats", "leave_date", "date")
+		change_column("bats", "collection_date", "date")
+		change_column("cages", "date_created", "date")
+		change_column("cages", "date_destroyed", "date")
+		change_column("MedicalProblems", "date_closed", "date")
+		change_column("users", "start_date", "date")
+		change_column("users", "end_date", "date")
+  end
+
+  def self.down
+  end
+end

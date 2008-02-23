a6649ecd862f3630e3f871b2dfd86df4cdfd465a
Ben Falk <falk.ben@gmail.com>
2008-02-23 18:25:50 +0000

    Working on training types

--- batkeeping/app/controllers/trianing_types_controller.rb
+++ batkeeping/app/controllers/trianing_types_controller.rb
@@ -1,3 +1,14 @@
 class TrainingTypesController < ApplicationController
-  
+	def index
+    list
+    render :action => 'list'
+  end
+	
+	def list
+    @training_types = TrainingType.find(:all)
+  end
+	
+	def new
+		@training_type = TrainingType.new
+	end
 end
